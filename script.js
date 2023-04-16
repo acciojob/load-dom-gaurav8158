5ab347ce1616e3a3f3b9bed1fcb244be1bc2b73c
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-04-16 16:59:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+window.addEventListener("DOMContentLoaded", (event) => {
+	console.log("DOM fully loaded and parsed");
+var ans = document.getElementById("body");  
+ans.innerText = "DOM load success";
+});
